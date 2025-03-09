5885568c689e87e927de2345861f3fb04c2334f4
Jiachen Yu <stephenyu1994@gmail.com>
2025-03-08 21:09:52 -0500

    projectupload

---
+++ customer_churn_project.py
@@ -0,0 +1,25 @@
+# Import Package
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+'''
+Goal:
+1. the primary reason of customer churn
+    1.1 what variable affect churn rate the most
+    1.2  Characteristics of churn customer
+    1.3  
+    
+'''
+
+# 1. Initial Data Exploration
+    # Load data set
+df = pd.read_csv('customer_churn_dataset-testing-master.csv')
+    # Display information and column types
+    # Checking Missing values
+
+# 2. Data Preproccessing & Feature
+
+
+
